dfa5926e2b5c270a6bad2b83aabbaf764dc20a30
dout231 <211599672+dout231q1@users.noreply.github.com>
2026-06-14 19:29:36 -0300

    feat: add campos autor isbn e anoPublicacao na entidade Livros

--- exercises/web/rest-api/livros-api/src/main/java/com/example/livrosapi/Livros.java
+++ exercises/web/rest-api/livros-api/src/main/java/com/example/livrosapi/Livros.java
@@ -1,30 +1,37 @@
 package com.example.livrosapi;
 
+
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 
+import java.time.Year;
+
 @Entity
-public class Livros {
+public class Livros{
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String titulo;
+    private String autor;
+    private String isbn;
+    private Year anoPublicacao;
 
-    public Livros() {}
-
-    public Livros(String titulo){
+    public Livros(Year anoPublicacao, String autor, String isbn, String titulo) {
+        this.anoPublicacao = anoPublicacao;
+        this.autor = autor;
+        this.isbn = isbn;
         this.titulo = titulo;
     }
 
     public Long getId() {return id;}
-    public void setId(Long id) {this.id = id;}
     public String getTitulo() {return titulo;}
     public void setTitulo(String titulo) {this.titulo = titulo;}
-
-    @Override
-    public String toString(){
-        return "ID: " + getId() + " | Nome: " + getTitulo();
-    }
+    public String getAutor() {return autor;}
+    public void setAutor(String autor) {this.autor = autor;}
+    public String getIsbn() {return isbn;}
+    public void setIsbn(String isbn) {this.isbn = isbn;}
+    public Year getAnoPublicacao() {return anoPublicacao;}
+    public void setAnoPublicacao(Year anoPublicacao) {this.anoPublicacao = anoPublicacao;}
 }
