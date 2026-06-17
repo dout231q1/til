e98230268fc6d4da57e5a3c18f3c291bce482379
dout231 <211599672+dout231q1@users.noreply.github.com>
2026-06-16 22:36:45 -0300

    refactor: implementa uso de DTOs na camada de controle de livros

--- exercises/web/rest-api/livros-api/src/main/java/com/example/livrosapi/controller/LivrosController.java
+++ exercises/web/rest-api/livros-api/src/main/java/com/example/livrosapi/controller/LivrosController.java
@@ -1,13 +1,14 @@
 package com.example.livrosapi.controller;
 
-import com.example.livrosapi.entity.Livros;
+import com.example.livrosapi.dto.LivrosRequestDTO;
+import com.example.livrosapi.dto.LivrosResponseDTO;
 import com.example.livrosapi.service.LivrosService;
-import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -17,32 +18,43 @@ import java.util.List;
 @RestController
 @RequestMapping("/livros")
 public class LivrosController {
-    @Autowired
-    private LivrosService livrosService;
+
+    private final LivrosService livrosService;
+
+    public LivrosController(LivrosService livrosService){
+        this.livrosService = livrosService;
+    }
 
     @PostMapping
-    public Livros adicionarLivros(@RequestBody Livros livro){
-        return livrosService.adicionarLivro(livro);
+    public ResponseEntity<LivrosResponseDTO> adicionarLivros(@RequestBody LivrosRequestDTO requestDTO){
+        LivrosResponseDTO novoLivro = livrosService.adicionarLivro(requestDTO);
+        return ResponseEntity.ok(novoLivro);
     }
 
     @GetMapping
-    public List<Livros> listarLivros(){
-        return livrosService.listarLivros();
+    public ResponseEntity<List<LivrosResponseDTO>> listarLivros(){
+        List<LivrosResponseDTO> livros = livrosService.listarLivros();
+        return ResponseEntity.ok(livros);
     }
 
     @GetMapping("/{id}")
-    public Livros buscarLivroPorId(@PathVariable Long id){
-        return livrosService.buscarLivro(id);
+    public ResponseEntity<LivrosResponseDTO> buscarLivroPorId(@PathVariable Long id){
+        LivrosResponseDTO livro = livrosService.buscarLivro(id);
+        if (livro == null){return ResponseEntity.notFound().build();}
+        return ResponseEntity.ok(livro);
     }
 
-    @PatchMapping("/{id}")
-    public Livros atualizarLivroPorId(@PathVariable Long id, @RequestBody Livros livro){
-        return livrosService.atualizarLivro(id, livro);
+    @PutMapping("/{id}")
+    public ResponseEntity<LivrosResponseDTO> atualizarLivro(@PathVariable Long id, @RequestBody LivrosRequestDTO requestDTO){
+        LivrosResponseDTO livroAtualizado = livrosService.atualizarLivro(id, requestDTO);
+        if(livroAtualizado == null){return ResponseEntity.notFound().build();}
+        return ResponseEntity.ok(livroAtualizado);
     }
 
     @DeleteMapping("/{id}")
     public void removerLivroPorId(Long id){
-        livrosService.excluirLivro(id);
+        livrosService.deletarLivro(id);
+        ResponseEntity.noContent().build();
     }
 
 
