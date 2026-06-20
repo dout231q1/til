a56e79a4d38b68aea99bf409c35b28df66722b0b
dout231 <211599672+dout231q1@users.noreply.github.com>
2026-06-19 22:38:37 -0300

    refactor: remove pastas antigas que sobraram na reestruturacao

--- exercises/web/rest-api/livros-api/src/main/java/com/example/livrosapi/dto/LivrosRequestDTO.java
+++
@@ -1,5 +0,0 @@
-package com.example.livrosapi.dto;
-
-import java.time.Year;
-
-public record LivrosRequestDTO(String titulo, String autor, String isbn, Year anoPublicacao){}
--- exercises/web/rest-api/livros-api/src/main/java/com/example/livrosapi/repository/LivrosRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.livrosapi.repository;
-
-import com.example.livrosapi.entity.Livros;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface LivrosRepository extends JpaRepository<Livros, Long> {
-}
