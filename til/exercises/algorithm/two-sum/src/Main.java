7e3f5567e86afd83b147260efef06cc97133e74d
dout231 <211599672+dout231q1@users.noreply.github.com>
2026-07-22 21:58:38 -0300

    add: exercicio de algoritmo two-sum

---
+++ til/exercises/algorithm/two-sum/src/Main.java
@@ -0,0 +1,24 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class Main {
+    public static void main(String[] args) {
+        Solution s = new Solution();
+        int[] result = s.twoSum(new int[]{2,7,11,13}, 9);
+        System.out.println(Arrays.toString(result));
+    }
+
+static class Solution {
+        public int[] twoSum(int[] nums, int target) {
+            for (int i = 0; i < nums.length; i++) {
+                for (int j = i + 1; j < nums.length; j++) {
+                    if (nums[i] + nums[j] == target) {
+                        return new int[]{i, j};
+                    }
+                }
+            }
+            return new int[]{};
+        }
+    }
+}
