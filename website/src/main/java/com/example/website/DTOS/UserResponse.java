4698a9e2975b5fd617a44de10808bfc356ca05b6
dout <211599672+dout231q1@users.noreply.github.com>
2026-07-22 21:42:13 -0300

    Delete website directory

--- website/src/main/java/com/example/website/DTOS/UserResponse.java
+++
@@ -1,9 +0,0 @@
-package com.example.website.DTOS;
-
-public record UserResponse(
-        Long id,
-        String name,
-        String email
-){}
-
-
--- website/src/main/java/com/example/website/Repositories/UserRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.website.Repositories;
-
-import com.example.website.Entities.UserEntity;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserRepository extends JpaRepository<UserEntity, Long> {
-}
